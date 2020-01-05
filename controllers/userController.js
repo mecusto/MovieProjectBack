ccb06b675f41a323265a2316b89680f73ebf03e4
Ignaciopardoc <ignaciopardocardona@gmail.com>
2020-01-05 10:51:36 +0100

    Added getUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,42 @@
 const mongoose = require('mongoose');
-//var bcrypt = require('bcryptjs');
-//var salt = 10;
+var bcrypt = require('bcryptjs');
+var salt = 10;
+const User = require("../user")
 
 const jwt = require("jsonwebtoken");
-
-//const User = require('../models/user.js');
-
 const myprivatekey = "tokenprivatekey";
 
 const userController = {};
 
+//Crear Usuario
+userController.createUser = (req, res) => {
+    const data = req.body
+    bcrypt.genSalt(salt, (err, salt) => {
+        bcrypt.hash(data.password, salt, (err, hash) => {
+            let passwordHashed = hash
+
+            User.create({ first_name: data.first_name, last_name: data.last_name, email: data.email, gender: data.gender, password: passwordHashed }, (err, user) => {
+                if (err) res.status(500).send("No se pudo crear el usuario")
+
+                res.status(200).send({ user })
+            })
+        });
+    });
+}
+
+//Devolver todos los usuarios
+userController.getUser = (req, res) => {
+    User.find({}, (err, results) => {
+        if (err) res.status(500).send({ message: "Error al obtener usuarios" })
+
+        res.status(200).send({ results })
+    })
+}
+
+//Autenticación
+//userController.auth = (req, res) => {
+
+//}
 
 //actualizar usuario
 userController.putUser = (req, res) => {
